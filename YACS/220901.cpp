acb7fb2f5e08901d2bcf0f48a6dbfde5bf13b468
Yang Yihang <yang.yihang@outlook.com>
2025-03-23 19:10:27 +0800

    Add first YACS problem

---
+++ YACS/220901.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main( ){
+   int a,b;
+   cin >> a >> b;
+   int s = a * b;
+   int l = ( a + b ) * 2;
+   cout << l << endl;
+   cout << s << endl;
+}
