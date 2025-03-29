53c5478b86626835cb0f08687746c2a8c2f73636
Yang Yihang <yang.yihang@outlook.com>
2025-03-29 13:09:13 +0800

    Add gitignore

---
+++ 3.2.3.6.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float f1 = 97.5;
+    float f2 = 97;
+    float f3 = 'a';
+    int i1 = 98;
+    int i2 = 98.6;
+    int i3 = 'b';
+    char c1 = 99;
+    char c2 = 99.1;
+    char c3 = 'c';
+    char c4 = 350;
+    cout << "f1="<< f1 <<endl;
+    cout << "f2="<< f2 <<endl;
+    cout << "f3="<< f3 <<endl;
+    cout << endl;
+    cout << "i1="<< i1 <<endl;
+    cout << "i2="<< i2 <<endl;
+    cout << "i3="<< i3 <<endl;
+    cout << endl;
+    cout << "c1="<< c1 <<endl;
+    printf("c1 = %d\n", c1);
+    cout << "c2="<< c2 <<endl;
+    cout << "c3="<< c3 <<endl;
+    cout << "c4="<< c4 <<endl;
+    cout << endl;
+    return 0;
+}
